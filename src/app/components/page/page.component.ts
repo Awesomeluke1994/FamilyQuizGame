4c9277767218eb60b2dfbeb9c74ad5a90df709d1
lukeliddell1994 <lukeliddell94@gmail.com>
2020-08-10 21:23:59 +0100

    Generated code for use

--- src/app/components/page/page.component.ts
+++ src/app/components/page/page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {DataItem} from "../table/table.component";
+import {DataItem} from '../table/table.component';
 import { Router } from '@angular/router';
 
 @Component({
